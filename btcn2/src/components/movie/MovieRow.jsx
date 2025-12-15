62100903a3828c23ba450a2d70183dc2877b8b48
23120328 <buuhuynhvinhphuc@gmail.com>
2025-12-15 12:14:22 +0700

    Hover vao movie se scale to len

--- btcn2/src/components/movie/MovieRow.jsx
+++ btcn2/src/components/movie/MovieRow.jsx
@@ -30,73 +30,71 @@ export default function MovieRow({ title, movies = [] }) {
 
     return (
         <div className="w-full flex flex-col p-2 max-w-[1200px]">
-        <h3 className="text-lg font-bold text-[#4b282d] dark:text-white mb-2">
-            {title}
-        </h3>
+            <h3 className="text-lg font-bold text-[#4b282d] dark:text-white mb-2">
+                {title}
+            </h3>
 
-        {/* SLIDE BAR */}
-        <div className="flex justify-center gap-2 mt-2 mb-1">
-            {pages.map((_, i) => (
-            <span
-                key={i}
-                className={`w-6 h-1.5 rounded ${
-                i === page ? "bg-red-500" : "bg-gray-300"
-                }`}
-            />
-            ))}
-        </div>
-
-        <div className="relative flex items-center">
-            {/* LEFT */}
-            <button
-            onClick={handlePrev}
-            className="absolute left-2 z-20 p-2 bg-white/80 dark:bg-black/60 rounded-full shadow-lg hover:scale-110 transition"
-            >
-            <ChevronLeft size={24} />
-            </button>
-
-            {/* VIEWPORT */}
-            <div className="overflow w-full px-12">
-                {/* TRACK */}
-                <div
-                    className="flex transition-transform duration-500 ease-out"
-                    style={{
-                    width: `${totalPages * 100}%`,
-                    transform: `translateX(-${page * (100 / totalPages)}%)`,
-                    }}
-                >
-                    {pages.map((pageMovies, index) => (
-                        <div
-                            key={index}
-                            className="w-full flex justify-center gap-4"
-                        >
-                            {pageMovies.map((movie) => (
-                                <div
-                                    key={movie.id}
-                                    className="w-[200px] aspect-[3/2] rounded-lg overflow-hidden shadow hover:scale-205 transition"
-                                >
-                                    <img
-                                    src={movie.image}
-                                    alt={movie.title}
-                                    className="w-full h-full object-fit"
-                                    />
-                                </div>
-                            ))}
-                        </div>
-                    ))}
-                </div>
+            {/* SLIDE BAR */}
+            <div className="flex justify-center gap-2 mt-2 mb-1">
+                {pages.map((_, i) => (
+                <span
+                    key={i}
+                    className={`w-6 h-1.5 rounded ${
+                    i === page ? "bg-red-500" : "bg-gray-300"
+                    }`}
+                />
+                ))}
             </div>
 
-            {/* RIGHT */}
-            <button
-            onClick={handleNext}
-            className="absolute right-2 z-20 p-2 bg-white/80 dark:bg-black/60 rounded-full shadow-lg hover:scale-110 transition"
-            >
-            <ChevronRight size={24} />
-            </button>
-        </div>
+            <div className="relative flex items-center">
+                {/* LEFT */}
+                <button
+                onClick={handlePrev}
+                className="absolute left-2 z-20 p-2 bg-white/80 dark:bg-black/60 rounded-full shadow-lg hover:scale-110 transition"
+                >
+                <ChevronLeft size={24} />
+                </button>
 
+                {/* VIEWPORT */}
+                <div className="overflow w-full px-12">
+                    {/* TRACK */}
+                    <div
+                        className="flex transition-transform duration-500 ease-out"
+                        style={{
+                        width: `${totalPages * 100}%`,
+                        transform: `translateX(-${page * (100 / totalPages)}%)`,
+                        }}
+                    >
+                        {pages.map((pageMovies, index) => (
+                            <div
+                                key={index}
+                                className="w-full flex justify-center gap-4"
+                            >
+                                {pageMovies.map((movie) => (
+                                    <div
+                                        key={movie.id}
+                                        className="w-[200px] aspect-[3/3] rounded-lg overflow-hidden shadow hover:scale-205 hover:z-50 transition"
+                                    >
+                                        <img
+                                        src={movie.image}
+                                        alt={movie.title}
+                                        className="w-full h-full object-fit"
+                                        />
+                                    </div>
+                                ))}
+                            </div>
+                        ))}
+                    </div>
+                </div>
 
+                {/* RIGHT */}
+                <button
+                onClick={handleNext}
+                className="absolute right-2 z-20 p-2 bg-white/80 dark:bg-black/60 rounded-full shadow-lg hover:scale-110 transition"
+                >
+                <ChevronRight size={24} />
+                </button>
+            </div>
         </div>
     );
 }
