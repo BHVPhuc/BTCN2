a3a64e057adb12b7af76b15cba857fae1071a410
23120328 <buuhuynhvinhphuc@gmail.com>
2025-12-15 11:47:11 +0700

    Lay danh sach Movie Popular tu API

--- btcn2/src/components/main/MainContent.jsx
+++ btcn2/src/components/main/MainContent.jsx
@@ -1,17 +1,45 @@
 import FeaturedMovie from "../movie/FeaturedMovie";
 import MovieRow from "../movie/MovieRow";
+import { movieService } from "../../services/movie.service";
+import { useEffect, useState } from "react";
+import { set } from "zod";
 
 export default function MainContent() {
+    const [popular, setPopular] = useState([]);
+    const [topRated, setTopRated] = useState([]);
+    const [featured, setFeatured] = useState(null);
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+        try {
+            const popularData = await movieService.getMostPopular({page: 1, limit: 12});
+            const topRatedData = await movieService.getTopRated({page: 1, limit: 12});
+            const featuredData = await movieService.getMostPopular({page: 1, limit: 5});
+        // console.log(popularData);
+        // console.log(topRatedData);
+            
+        setPopular(popularData.data || []);
+        setTopRated(topRatedData.data || []);
+        setFeatured(featuredData.data || []);
+            // setFeatured(popularData[0]); // phim nổi bật
+        } catch (error) {
+            console.error(error);
+        }
+        };
+
+        fetchMovies();
+    }, []);
+
     return (
-        <div className="flex-1 grid grid-rows-4 gap-2 py-6">
+        <div className="flex-1 grid grid-rows-4 gap-2 py-6 max-w-full">
             <div className="row-span-2">
-                <FeaturedMovie />
+                <FeaturedMovie movies={featured}/>
             </div>
             <div className="row-span-1">
-                <MovieRow title="Most Popular" />
+                <MovieRow title="Most Popular" movies={popular}/>
             </div>
             <div className="row-span-1">
-                <MovieRow title="Top Rating" />
+                <MovieRow title="Top Rating" movies={topRated}/>
             </div>
         </div>
     )
---
+++ btcn2/src/services/movie.service.js
@@ -0,0 +1,38 @@
+const API_BASE_URL = "api"; // sử dụng proxy trong vite.config.js
+// const APP_TOKEN = import.meta.env.VITE_APP_TOKEN; // nên để trong .env
+const APP_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IjIzXzMxIiwicm9sZSI6InVzZXIiLCJhcGlfYWNjZXNzIjp0cnVlLCJpYXQiOjE3NjUzNjE3NjgsImV4cCI6MTc3MDU0NTc2OH0.O4I48nov3NLaKDSBhrPe9rKZtNs9q2Tkv4yK0uMthoo"; 
+
+const headers = {
+  "Content-Type": "application/json",
+  "x-app-token": APP_TOKEN,
+};
+
+export const movieService = {
+  async getMostPopular({page = 1, limit = 12} = {}) {
+    const res = await fetch(`${API_BASE_URL}/movies/most-popular?page=${page}&limit=${limit}`, {
+      headers,
+    });
+    if (!res.ok) throw new Error("Failed to fetch popular movies");
+    return res.json();
+  },
+
+  async getTopRated({page = 1, limit = 12, category = "IMDB_TOP_50"} = {}) {
+    const res = await fetch(`${API_BASE_URL}/movies/top-rated?category=${category}&page=${page}&limit=${limit}`, {
+      headers,
+    });
+    if (!res.ok) throw new Error("Failed to fetch top rated movies");
+    return res.json();
+  },
+
+  async getAll({page = 1, limit = 10}) {
+    const res = await fetch(`${API_BASE_URL}/movies?page=${page}&limit=${limit}`, { headers });
+    if (!res.ok) throw new Error("Failed to fetch movies");
+    return res.json();
+  },
+
+  async getDetail(id) {
+    const res = await fetch(`${API_BASE_URL}/movies/${id}`, { headers });
+    if (!res.ok) throw new Error("Failed to fetch movie detail");
+    return res.json();
+  },
+};
