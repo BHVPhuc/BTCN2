5e1f0ae66deb34ce1d9d84858ec986d3161482f6
23120328 <buuhuynhvinhphuc@gmail.com>
2025-12-15 20:05:56 +0700

    Loading Home: them trang thai khi page load

--- btcn2/src/components/main/MainContent.jsx
+++ btcn2/src/components/main/MainContent.jsx
@@ -7,26 +7,28 @@ export default function MainContent() {
     const [popular, setPopular] = useState([]);
     const [topRated, setTopRated] = useState([]);
     const [featured, setFeatured] = useState([]);
-
+    const [loading, setLoading] = useState(true);
 
 
     useEffect(() => {
         const fetchMovies = async () => {
-        try {
-            const popularData = await movieService.getMostPopular({page: 1, limit: 12});
-            const popularData2 = await movieService.getMostPopular({page: 2, limit: 3});
-            const topRatedData = await movieService.getTopRated({page: 1, limit: 12, category: "IMDB_TOP_50"});
-            const topRatedData2 = await movieService.getTopRated({page: 2, limit: 3, category: "IMDB_TOP_50"});
-            const featuredData = await movieService.getMostPopular({page: 1, limit: 5});
+            try {
+                const popularData = await movieService.getMostPopular({page: 1, limit: 12});
+                const popularData2 = await movieService.getMostPopular({page: 2, limit: 3});
+                const topRatedData = await movieService.getTopRated({page: 1, limit: 12, category: "IMDB_TOP_50"});
+                const topRatedData2 = await movieService.getTopRated({page: 2, limit: 3, category: "IMDB_TOP_50"});
+                const featuredData = await movieService.getMostPopular({page: 1, limit: 5});
 
-        popularData.data.push(...popularData2.data);
-        topRatedData.data.push(...topRatedData2.data);
-        setPopular(popularData.data || []);
-        setTopRated(topRatedData.data || []);
-        setFeatured(featuredData.data || []);
-        } catch (error) {
-            console.error(error);
-        }
+                popularData.data.push(...popularData2.data);
+                topRatedData.data.push(...topRatedData2.data);
+                setPopular(popularData.data || []);
+                setTopRated(topRatedData.data || []);
+                setFeatured(featuredData.data || []);
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setLoading(false);
+            }
         };
 
         fetchMovies();
